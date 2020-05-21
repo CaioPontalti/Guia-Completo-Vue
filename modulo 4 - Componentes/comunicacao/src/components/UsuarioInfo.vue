62503ab0c55bd2de7a868b76d7cb11a63dc2c993
Caio Pontalti <caiopontalti@hotmail.com>
2020-05-21 16:17:47 -0300

    finalizando desafio componente

---
+++ modulo 4 - Componentes/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do Usuário: <strong>{{ nome }}</strong></p>
+        <p>Idade do Usuário: {{ idade }}</p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
+    </div>
+</template>
+
+<script>
+
+// import barramento from '@/barramento'
+
+export default {
+    props:{
+        nome:{ 
+            type: String, 
+            //required:true,
+            default: function(){
+                return 'teste';
+            }
+        },
+        idade: {type: Number}
+    },
+    methods:{
+        reiniciarNome(){
+            this.nome = 'Reiniciou nome no filho';
+            this.$emit('mudouNomeFilho', this.nome)
+        }
+    },
+    // created(){
+    //     barramento.$on('alterarIdade', idade =>{
+    //         this.idade = idade
+    //     })
+    // }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
