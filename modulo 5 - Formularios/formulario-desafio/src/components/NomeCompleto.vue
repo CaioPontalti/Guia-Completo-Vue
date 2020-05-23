0e23fd4ffcbfb1436111c87315c4d14f155cc8d3
Caio Pontalti <caiopontalti@hotmail.com>
2020-05-23 13:56:54 -0300

    finalizando desafio componentes

---
+++ modulo 5 - Formularios/formulario-desafio/src/components/NomeCompleto.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+
+        <rotulo nome="Nome">
+            <input type="text" :value="value.nome" @input="alterarNome">
+        </rotulo>
+        
+        <rotulo nome="Sobre Nome">
+            <input type="text" :value="value.sobreNome" @input="alterarSobreNome">
+        </rotulo>
+
+    </div>
+</template>
+
+<script>
+
+import Rotulo from './Rotulo.vue'
+
+export default {
+    props:{
+        value:{ type: Object, required: true}
+    },
+    components:{
+        Rotulo
+    },
+    methods:{
+        alterarNome(event){
+            this.$emit('input', { nome: event.target.value, sobreNome: this.value.sobreNome })
+        },
+        alterarSobreNome(event){
+            this.$emit('input', { nome: this.value.nome, sobreNome: event.target.value })
+        }
+    }
+    
+}
+</script>
