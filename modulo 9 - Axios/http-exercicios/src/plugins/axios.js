a0aff82f7cfb81c5ab911f849597db233a2e656f
Caio Pontalti <caiopontalti@hotmail.com>
2020-05-26 13:09:21 -0300

    continuacao axios

--- modulo 9 - Axios/http-exercicios/src/plugins/axios.js
+++ modulo 9 - Axios/http-exercicios/src/plugins/axios.js
@@ -4,7 +4,21 @@ import axios from 'axios'
 Vue.use({
     install(Vue){
         Vue.prototype.$http = axios.create({
-            baseURL: 'https://curso-vue-277213.firebaseio.com/'
+            baseURL: 'https://curso-vue-277213.firebaseio.com/',
+            headers:{
+                "Authorization": "Token"
+            }
         })
+
+        Vue.prototype.$http.interceptors.request.use(config =>{
+            console.log(config.method)
+
+            return config
+        }, error => Promise.reject(error))
+
+        // Vue.prototype.$http.interceptors.response.use(res=>{
+
+        // })
+
     }
 })
