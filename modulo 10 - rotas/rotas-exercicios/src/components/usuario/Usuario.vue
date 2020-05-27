cfebe148c6c7a9c3eb0ebef368543e494f13a53b
Caio Pontalti <caiopontalti@hotmail.com>
2020-05-27 12:34:23 -0300

    continuação rotas

--- modulo 10 - rotas/rotas-exercicios/src/components/usuario/Usuario.vue
+++ modulo 10 - rotas/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -2,26 +2,15 @@
     <div class="usuario">
         <h2>Usuários</h2>
         <hr>
-        <p><strong>Código: </strong>{{ id }}</p>
+        <router-view/>
         <button sucesso @click="irParainicio">Voltar</button>
     </div>
 </template>
 <script>
 export default {
-    // data(){
-    //     return{
-    //         id: this.$route.params.id
-    //     }
-    // }, 
-    // watch:{
-    //     $route(novo, antigo){
-    //         this.id = novo.params.id
-    //     }
-    // },
-    props:['id'],
     methods:{
         irParainicio(){
-            this.$router.push('/')
+            this.$router.push('/usuario')
         }
     }
 }
